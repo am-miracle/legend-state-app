1874d96c5b5a4a2884961d1a81054c7db7b6262d
Miracle Jude <judemiraco0@gmail.com>
2022-10-14 23:34:03 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, {useRef, useState } from 'react';
+import React, {useRef} from 'react';
 import './App.css';
-// import Card from './components/Card';
-import { observable, observe } from '@legendapp/state';
-import { enableLegendStateReact, Memo } from '@legendapp/state/react'
+import { observable } from '@legendapp/state';
+import { enableLegendStateReact, observer } from '@legendapp/state/react'
+import Card from './components/Card';
 
 enableLegendStateReact();
 
 const state = observable({
-  bestPlayers: [
+  players: [
     {id: 1, name: 'Messi', club: 'Paris', country: 'ARG',},
     {id: 2, name: 'Ronaldo', club: 'Manchester', country: 'POR'}
   ],
-  isVoted: false,
   voteForM: 0,
   voteForC: 0
 })
@@ -19,46 +18,40 @@ const state = observable({
 function App() {
   const renderCount = ++useRef(0).current;
 
-  const playersData = state.bestPlayers.get();
-  // const playersId = state.bestPlayers.get().map(playerId => playerId.id);
+  const playersData = state.players.get();
 
+  observer(() => {
+    console.log(state.voteForM.get());
+    console.log(state.voteForC.get());
+  })
+  state.voteForM.onChange((messi) => console.log('Number of votes is', messi))
 
   const voteForMessi = () =>  state.voteForM.set(state.voteForM.get() + 1)
   const voteForRonaldo = () => state.voteForC.set(state.voteForC.get() + 1)
   const unVoteForMessi = () =>  state.voteForM.set(state.voteForM.get() - 1)
   const unVoteForRonaldo = () => state.voteForC.set(state.voteForC.get() - 1)
 
-
   return (
-    <div className="App">
+    <section className='App'>
       <h2>{renderCount}</h2>
-      Vote the Best player in the world
-      <Memo>
-            <h1>Messi: {state.voteForM} - {state.voteForC}: Ronaldo</h1>
-      </Memo>
-      {playersData.map((player) => (
-        <div key={player.id}>
-          <h1>{player.name}</h1>
-          <p>{player.country}</p>
-          <h3>{player.club}</h3>
-          <div>
-            <button
-            onClick={
-               player.id === 1 ? voteForMessi : voteForRonaldo
-            }
-              >Vote</button>
-            <button
-              disabled={
-                (player.id === 1 && state.voteForM.get() <= 0 ? true : false) ||
-                (player.id === 2 && state.voteForC.get() <= 0 ? true : false)
-              }
-              onClick={
-                player.id === 1 ? unVoteForMessi : unVoteForRonaldo
-              }>Unvote</button>
-          </div>
-        </div>
-      ))}
-    </div>
+      <h2 style={{ marginBottom: "20px"}}>Vote the Best Football player in the world</h2>
+      {/* <Memo> */}
+        <h1>Messi: {state.voteForM} - {state.voteForC}: Ronaldo</h1>
+      {/* </Memo> */}
+      <div className='card-container'>
+        {
+          playersData.map((player) => (
+            <div key={player.id} className="card">
+              <Card
+               player={player}
+               increasevoteCount={player.id === 1 ? voteForMessi : voteForRonaldo}
+               decreaseVoteCount={player.id === 1 ? unVoteForMessi : unVoteForRonaldo}
+              />
+            </div>
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,17 @@
-import React, { useEffect } from "react"
+import React from "react"
 
-const Card = ({item}) => {
-    useEffect(() => {
-        item.renders.set(r => r + 1);
-    })
+const Card = (props) => {
+    const { player, increasevoteCount, decreaseVoteCount } = props
+
     return (
-        <div>
-            {item.text}
-        </div>
+        <section className="container">
+            <h1 className="text">{player.name}</h1>
+            <h3 className="normal-text">{player.country}</h3>
+            <p className="normal-text">{player.club}</p>
+            <button className="button" onClick={() => increasevoteCount(player.id)}>Vote</button>
+            <button className="button normal" onClick={() => decreaseVoteCount(player.id)}>Unvote</button>
+        </section>
     )
-}
+};
 
 export default Card;
